2b5409acf148cb3d0db3d0b88ce5b20de45c9da8
shameershaik28 <102400953+shameershaik28@users.noreply.github.com>
2024-12-07 18:41:52 +0530

    first commit

---
+++ src/main/java/com/cricket/assignment/CricketAssignment.java
@@ -0,0 +1,7 @@
+package com.cricket.assignment;
+
+public class CricketAssignment {
+    public static void main(String[] args) {
+        System.out.println("Hello, Cricket Assignment!");
+    }
+}
